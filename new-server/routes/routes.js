339cbe347761f8b2857096226d14edc46d7f63ce
Dillon Means <dillon.j.means@gmail.com>
2018-05-25 14:48:17 -0600

    final backend

--- new-server/routes/routes.js
+++ new-server/routes/routes.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const knex = require('../knex');
 
 
+
+function validPickup(pickup) {
+  const hasPickup = typeof pickup.p_u_line == 'string' && pickup.p_u_line.trim() != ''
+  const hasCategory = typeof pickup.category == 'string' && pickup.category.trim() != ''
+  const hasRate = typeof pickup.success_rate == 'number' && pickup.successrate.trim() != ''
+  return hasPickup && hasCategory && hasRate
+}
+
 router.get('/', (req, res) => {
    knex('pickups')
   .then(pickups => res.json(pickups))
@@ -14,12 +22,23 @@ router.get('/:id', (req, res) =>{
   .then(data => res.json(data))
 })
 
-router.post('/', (req, res) => {
-  knex('pickups').insert(req.body, '*')
-  .then(pickups =>{
-    res.json(pickups)
-  })
+router.post('/', (req, res, next) => {
+  if(validPickup(req.body)) {
+    knex('pickups').insert(req.body, '*')
+    .then(pickups =>{
+      res.json(pickups)
+    })
+  } else {
+    next(new Error('Invalid Pickup'))
+  }
 })
+
+// router.post('/', (req, res) => {
+//   knex('pickups').insert(req.body, '*')
+//   .then(pickups =>{
+//     res.json(pickups)
+//   })
+// })
 
 router.put('/:id', (req, res) =>{
   knex('pickups').where('id', req.params.id).update(req.body, '*')
