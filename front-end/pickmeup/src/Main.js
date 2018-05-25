a6b36e61694f340dca0e4a70974ae599f04d8d77
Brevon Good <brevonbgood@comcast.net>
2018-05-25 15:28:26 -0600

    update

--- front-end/pickmeup/src/Main.js
+++ front-end/pickmeup/src/Main.js
@@ -8,7 +8,7 @@ export class Main extends React.Component {
     return (
       <div className='app-main'>
         <div className='sub-header'>
-          <h2 className='slogan'>"You're going to be drowning in trim, we guarentee it"</h2>
+          <h2 className='slogan'>"You're going to be drowning in trim, we guarantee it"</h2>
           <div className='filter-buttons'>
             <ul>
               <li></li>
--- front-end/pickmeup/src/PickUpLine.js
+++ front-end/pickmeup/src/PickUpLine.js
@@ -5,9 +5,9 @@ export class PickUpLine extends React.Component {
   render() {
     return (
       <div className='pick-up-line'>
-        <span>Category: {this.props.pickUpLine.category}</span>
-        <h3>{this.props.pickUpLine.p_u_line}</h3>
-        <p>{this.props.pickUpLine.success_rate}%</p>
+        <span className='category'>Category: {this.props.pickUpLine.category}</span>
+        <h3 className='line'>{this.props.pickUpLine.p_u_line}</h3>
+        <p className='success'>Success Rate: {this.props.pickUpLine.success_rate}%</p>
       </div>
     )
   }
