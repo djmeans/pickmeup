a7f51cdfb833e2a206aaa400157ab2b1774d3b8c
Christopher Nemeth <cnemeth1@gmail.com>
2018-05-25 13:07:42 -0600

    started footer component

---
+++ front-end/pickmeup/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Footer extends React.Component {
+  render() {
+    return(
+      <div className='footer'>
+        <h2 className=''></h2>
+      </div>
+    )
+  }
+}
